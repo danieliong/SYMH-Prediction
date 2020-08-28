11585302344193a8618d6f1b6481393859c33b14
Daniel Iong <daniong@daniong-mbp.local>
2020-08-28 11:36:01 -0400

    made tuning script more robust

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -12,13 +12,21 @@ from GeoMagTS.data_preprocessing import DataFrameSelector, timeResolutionResampl
 from GeoMagTS.models import GeoMagTSRegressor, GeoMagARX
 from GeoMagTS.utils import create_narx_model, trainTestStormSplit
 
+import os 
 from os import path
 import joblib
 
-DATA_FILE = '../../data/omni_2010-2019.pkl'
-STORMTIMES_FILE = '../../data/stormtimes_qusai.pkl'
 DIR = 'tuning/'
 
+N_JOBS = -1
+TEST = True
+RUN_NUM = 1
+MODELS_TO_TUNE = ['linear']
+
+DATA_FILE = '../../data/omni_2010-2019.pkl'
+STORMTIMES_FILE = '../../data/stormtimes_qusai.pkl'
+
+# Read data
 data = pd.read_pickle(DATA_FILE)
 storm_times_df = pd.read_pickle(STORMTIMES_FILE)
 
@@ -27,7 +35,7 @@ storm_times_df = pd.read_pickle(STORMTIMES_FILE)
 # Data pre-processing parameters
 time_resolution = '5T'
 target_column = 'sym_h'
-feature_columns = ['b', 'by_gse', 'vz_gse', 'density']
+feature_columns = ['by', 'bz', 'vx_gse', 'density']
 columns = [target_column] + feature_columns
 storms_to_delete = [15, 69, 124]
 storms_to_use = np.where(~np.isin(storm_times_df.index, storms_to_delete))
@@ -50,73 +58,94 @@ storm_times = data_pipeline['storms_processor'].get_times()
 n_storms = len(set(storm_labels))
 
 # Split data into train, test
-min_threshold = -100
+test_storms = [61,62,78,79]
+# min_threshold = -100
+# train_test_split = trainTestStormSplit(storm_labels=storm_labels,
+#                                        min_threshold=min_threshold, y=y)
 train_test_split = trainTestStormSplit(storm_labels=storm_labels,
-                                       min_threshold=min_threshold, y=y)
+                                       test_storms=test_storms)
 X_train, y_train, X_test, y_test = train_test_split.split_data(X, y)
 storm_labels_train, storm_labels_test = train_test_split.split_storm_labels()
 storm_times_test = train_test_split.get_test_storm_times(storm_times)
 
 cv = GroupKFold()
-
-# Test 
-# param_grid_test = {
-#     'pred_step': [24],
-#     'transformer_X': [RobustScaler()],
-#     'transformer_y': [None, RobustScaler()],
-#     'auto_order': [18, 24],
-#     'exog_order': [18, 24]
-# }
-# ar_model = GeoMagARX()
-# gridsearch_test = GridSearchCV(estimator=ar_model, 
-#                              param_grid=param_grid_test, 
-#                              cv=cv, n_jobs=-3)
-# gridsearch_test.fit(X_train, y_train, 
-#                   groups=storm_labels_train)
-# joblib.dump(gridsearch_test, DIR+'test.pkl')
-
 ### Set parameter grids
 param_grid = {
-    'pred_step': [36],
+    'pred_step': [24],
     'transformer_X': [RobustScaler()],
     'transformer_y': [None, RobustScaler()],
-    'auto_order': [12, 18, 24, 30],
+    'auto_order': [0, 16, 24, 30],
     'exog_order': [12, 18, 24, 30],
 }
-param_grid_ar = param_grid.copy()
-param_grid_narx = param_grid.copy()
+param_grid_test = {
+    'pred_step': [24],
+    'transformer_X': [RobustScaler()],
+    'transformer_y': [None],
+    'auto_order': [16, 24],
+    'exog_order': [18],
+}
 # IDEA: Write function that updates a previously run GridSearchCV
 
-# Linear AR-X
-ar_model = GeoMagARX()
-if not path.exists(DIR+'ar.pkl'):
-    gridsearch_ar = GridSearchCV(estimator=ar_model,
-                                param_grid=param_grid_ar,
-                                cv=cv, n_jobs=-3, verbose=1)
-    gridsearch_ar.fit(X_train, y_train,
-                        groups=storm_labels_train)
-    joblib.dump(gridsearch_ar, DIR+'ar.pkl')
+# Make dir for current run and save information to a text file
+while path.exists(DIR+"run"+str(RUN_NUM)+"/"):
+    RUN_NUM = RUN_NUM + 1
+run_dir = DIR+"run"+str(RUN_NUM)+"/"
+os.mkdir(run_dir)
 
-# NN-ARX
-base_estimator = KerasRegressor(build_fn=create_narx_model)
-narx_model = GeoMagTSRegressor(base_estimator=base_estimator,
-                                n_hidden=10, learning_rate=.01)
-if not path.exists(DIR+'narx.pkl'):
-    param_grid_narx.update(
-        {'base_estimator__n_hidden': [12, 16, 18, 20, 24, 30],'base_estimator__learning_rate': [.001, .005, .01, .05, .1, .5]}
-        )
-    gridsearch_narx = GridSearchCV(estimator=narx_model,
-                                param_grid=param_grid_narx,
-                                cv=cv, n_jobs=-3, verbose=1)
-    gridsearch_narx.fit(X_train, y_train,
-                    groups=storm_labels_train)
-    joblib.dump(gridsearch_narx, DIR+'narx.pkl')
+info_file = open(run_dir+"info.txt", "a")
+info_file.write("RUN #: "+str(RUN_NUM)+'\n')
+info_file.write("-------------------------------------------"+'\n')
+info_file.write("TEST: "+str(TEST)+'\n')
+info_file.write("MODELS_TO_TUNE: "+', '.join(MODELS_TO_TUNE)+'\n')
+info_file.write("DATA_FILE: "+DATA_FILE+'\n')
+info_file.write("STORMTIMES_FILE: "+STORMTIMES_FILE+'\n')
+info_file.write("\n")
+info_file.write('Time resolution: '+time_resolution+'\n')
+info_file.write('Feature columns: '+', '.join(feature_columns)+'\n')
+info_file.write('Storms deleted: '+str(storms_to_delete)+'\n')
+info_file.write('# of storms: '+str(n_storms)+'\n')
+info_file.write('Test storms: '+str(test_storms)+'\n')
+info_file.write('\n')
+info_file.close()
+
+for model in MODELS_TO_TUNE:
+    if TEST:
+        fname = run_dir+'gs_'+model+'_test'+str(RUN_NUM)+'.pkl'
+        param_grid_model = param_grid_test.copy()
+    else:
+        fname = run_dir+'gs_'+model+'_'+str(RUN_NUM)+'.pkl'
+        param_grid_model = param_grid.copy()
+    if model == 'linear':
+        # Linear AR-X
+        model = GeoMagARX()
+    elif model == 'nn':
+        # Neural Network AR-X
+        narx = KerasRegressor(build_fn=create_narx_model)
+        narx_model = GeoMagTSRegressor()
+        if TEST:
+            param_grid_model.update(
+                {'base_estimator': [narx],
+                'base_estimator__n_hidden': [18], 'base_estimator__learning_rate': [.001, .01]}
+            )
+        else:
+            param_grid_model.update(
+                {'base_estimator': [narx],
+                'base_estimator__n_hidden': [12, 16, 18, 20, 24, 30], 'base_estimator__learning_rate': [.001, .005, .01, .05, .1, .5]}
+            )
+    # Perform grid search CV    
+    if not path.exists(fname):
+        gridsearch = GridSearchCV(estimator=model,
+                                    param_grid=param_grid_model,
+                                    cv=cv, n_jobs=N_JOBS, verbose=0)
+        gridsearch.fit(X_train, y_train,
+                            groups=storm_labels_train)
+        joblib.dump(gridsearch, fname)
 
 
-gridsearch_ar = joblib.load(DIR+'ar.pkl')
-ar_model.set_params(**gridsearch_ar.best_params_)
-ar_model.fit(X_train, y_train)
-ar_model.plot_predict(X_test, y_test, 
-                      times=storm_times_test,
-                      display_info=True)
-ar_model.get_coef_df(feature_columns)
+# gridsearch_ar = joblib.load(DIR+'ar.pkl')
+# ar_model.set_params(**gridsearch_ar.best_params_)
+# ar_model.fit(X_train, y_train)
+# ar_model.plot_predict(X_test, y_test, 
+#                       times=storm_times_test,
+#                       display_info=True)
+# ar_model.get_coef_df(feature_columns)
